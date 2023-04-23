c2e75d68ae1bedb3718170dedfa3c90f1408e306
Arpan Mathe <mathe.arpan@gmail.com>
2023-04-23 21:53:47 +0200

    Random message generator - First upload

---
+++ message_generator.js
@@ -0,0 +1,35 @@
+const subjects = [
+    "Yourself",
+    "Your friend",
+    "Your family",
+    "Your pet",
+    "Your boss",
+  ];
+  
+  const verbs = [
+    "needs",
+    "deserves",
+    "wants",
+    "requires",
+    "should have",
+  ];
+  
+  const objects = [
+    "love",
+    "support",
+    "kindness",
+    "understanding",
+    "patience",
+  ];
+
+
+function message_genrator(){
+    let ran = (Math.ceil(Math.random()*5)-1)
+    // console.log(ran)
+    output = (subjects[ran]+ " " + verbs[ran] + " " + objects[ran])
+
+    return output
+}
+
+
+console.log(message_genrator())
